001d24d9eea644852334f081faa2436507767e13
urbanoyahoo <urbanofilho74@yahoo.com>
2023-02-02 23:27:13 -0300

    view das perguntas

---
+++ GUIAPERGUNTAS/database/database.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('sistemaDeCadastro', 'admin','root',{
+    host: '127.0.0.1',
+    port: '3307',
+    dialect: 'mysql'
+});
+
+module.exports = connection;
--- GUIAPERGUNTAS/index.js
+++ GUIAPERGUNTAS/index.js
@@ -1,16 +1,67 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require('./database/database');
+const pergunta = require("./database/Pergunta");
+const Pergunta = require("./database/Pergunta");
+
+connection
+    .authenticate()
+    .then(() =>{
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get("/",(req, res) =>{
-    res.render("index");
+    Pergunta.findAll({raw: true, order:[
+        ['id','DESC']
+    ]}).then(perguntas =>{
+        res.render("index",{
+            perguntas: perguntas
+        });
+    })
+
 });
 
 app.get("/perguntar",(req, res) =>{
     res.render("perguntar");
 });
+
+app.post("/salvarpergunta",(req, res) =>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(()=> {
+        res.redirect("/");
+    });
+});
+
+app.get("/pergunta/:id",(req, res) =>{
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined){
+           res.render('pergunta',{
+            pergunta: pergunta
+           }) ;
+        }else{
+            res.redirect("/");
+        }
+    })
+})
 
 
 app.listen(8088, () => {
