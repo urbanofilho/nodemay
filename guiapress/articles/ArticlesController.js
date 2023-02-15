e4a00563f6143e31bb2d63fa413d42cece3d1508
urbanoyahoo <urbanofilho74@yahoo.com>
2023-02-15 00:21:12 -0300

    criação do artigo

--- guiapress/articles/ArticlesController.js
+++ guiapress/articles/ArticlesController.js
@@ -51,4 +51,42 @@ router.post("/articles/delete", (req, res) => {
     }
 });
 
+router.get("/admin/articles/edit/:id",(req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+    Article.findByPk(id,{
+        include: [{model: Category}]
+    }).then(article => {
+        if(article != undefined){
+
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit",{article: article, categories: categories})
+            });
+
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/articles");
+    })
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title: title, slug: slugify(title), body: body,categoryId: category},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
+})
+
 module.exports = router;
